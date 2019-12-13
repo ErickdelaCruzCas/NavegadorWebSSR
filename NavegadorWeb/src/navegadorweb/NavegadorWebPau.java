1a674843e7420916569a45be88d8d0a3608811bf
alejandalb98 <58817715+alejandalb98@users.noreply.github.com>
2019-12-13 17:01:54 +0100

    Imagen ya va
    y tambien encoding

--- NavegadorWeb/src/navegadorweb/NavegadorWebPau.java
+++ NavegadorWeb/src/navegadorweb/NavegadorWebPau.java
@@ -10,11 +10,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Base64;
 import java.util.Scanner;
@@ -24,37 +26,38 @@ import javax.imageio.stream.ImageInputStream;
 
 public class NavegadorWebPau {
 
-	public static String descargarImagen(String path) throws IOException {
-		String path1 = "C:/Users/erick/Documents/NavegadorWebs/";
-		String pathDir = getSubstring("/", path);
-		File file = new File(path1 + pathDir);
+	public static void descargarImagen(String path) throws IOException {
+		String path1 = "C:/Users/Usuario/Desktop/PruebasNavegador";
+		String nombreImagen = getSubstring("/", path);
+		File file = new File(path1 + path.replace(nombreImagen,""));
 		file.mkdirs();
 		File imagen = new File(path1 + path);
-		URL obj = new URL("http://localhost:8080/" + path);
+		URL obj = new URL("https://www.google.es:443" + path);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		con.setRequestMethod("GET");
 		BufferedImage image = ImageIO.read(obj);
 		ImageIO.write(image, "png", imagen);
-		return imagen.getPath();
+		System.out.println(" [OK] " +  nombreImagen);
 
 	}
 
 	public static String getSubstring(String a, String b) {
-		String substring = b.split(a)[0];
+		String[] splitres = b.split(a);
+		String substring = splitres[splitres.length - 1];
+		
 		return substring;
 	}
 
 	public static void main(String[] args) throws IOException {
-		URL obj = new URL("http://localhost:8080");
+		URL obj = new URL("https://www.google.es:443");
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		con.setRequestMethod("GET");
-		String path = "C:/Users/erick/Documents/NavegadorWebs/argumentos.html";
+		String path = "C:/Users/Usuario/Desktop/PruebasNavegador/argumentos.html";
 		File file = new File(path);
-		BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter( new FileOutputStream(file), StandardCharsets.UTF_8));
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String inputLine;
 		String urlimg;
-		String nuevoPath;
 		while ((inputLine = in.readLine()) != null) {
 			if (inputLine.contains("<img") && inputLine.contains("src")) {
 				urlimg = "";
@@ -63,8 +66,9 @@ public class NavegadorWebPau {
 						urlimg = inputLine.substring(i);
 						urlimg = urlimg.split("\"")[1];
 						//System.out.println(urlimg);
-						nuevoPath = descargarImagen(urlimg);
-						inputLine.replaceAll(urlimg, nuevoPath);
+						descargarImagen(urlimg);
+						inputLine = inputLine.replace(urlimg, urlimg.substring(1));
+						//System.out.println(inputLine.replace(urlimg, urlimg.substring(1)));
 					}
 				}
 			}
